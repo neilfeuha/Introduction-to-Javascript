9e124c89d79c00f3e2b7c82d31bd58be06610fbc
neilfeuha <feuhaneil@gmail.com>
2022-06-22 17:38:24 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let userName = window.prompt('What is your name?');
+let height = window.prompt('What is your height in metres?');
+let country = window.prompt('What country do you come from?');
+
+document.write(userName+' is '+height+' metres tall and is from the country '+country);
+
+
+
+
